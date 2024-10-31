e88d4286db10413def49d5c0aa60a0a3a13d030d
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2024-10-31 23:18:18 +0100

    add feature/list of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mentoring-program-starter-kit';
+  httpClient = inject(HttpClient);
+  categoriesColors: string[] = ['btn py-1 btn-primary mx-3', 'btn py-1 btn-secondary', 'btn py-1 btn-success mx-3', 'btn py-1 btn-danger'];
+  categories$: Observable<string[]> = this.httpClient.get<string[]>('https://fakestoreapi.com/products/categories');
+  products$: Observable<any[]> = this.httpClient.get<any[]>('https://fakestoreapi.com/products')
 }
